62aca38bcb42e1386176d653329c0d79053b5067
CDL <Zhuanz@MacBook-Air.local>
2026-05-29 09:32:32 +0800

    配置更新：Astro wrangler 依赖升级

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://chendaolei.us.kg',
   output: 'static',
+  integrations: [sitemap()],
 });
